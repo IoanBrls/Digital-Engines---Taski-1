39912582597549f718ac8c563f56d0e490bf2bf1
IoanBrls <giannhsmprls@gmail.com>
2019-10-19 02:25:39 +0200

    The original code

---
+++ Assets/Scripts/SinCube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinCube : MonoBehaviour
+{
+	public bool animate;
+	public float vortex = 0f;
+	public float magnitude = 0;
+	private float timer = 0f;
+	private Vector3 direction = new Vector3(0, 0.5f, 0);
+    // Update is called once per frame
+    void Update()
+    {
+		if (animate)
+		{
+			if (vortex != 0f)
+			{
+				timer += Time.deltaTime * 2;
+				transform.position += Vector3.up * Mathf.Sin(timer + Mathf.PI/2) * Time.deltaTime * magnitude * vortex;
+			}
+			else
+			{
+				timer += Time.deltaTime * 2;
+				transform.position += Vector3.up * Mathf.Sin(timer + Mathf.PI/2) * Time.deltaTime * magnitude;
+			}
+			
+		}
+		else
+		{
+			timer = 0f;
+		}
+			
+    }
+}
---
+++ Assets/Scripts/SinWave.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinWave : MonoBehaviour
+{
+	public GameObject Cube_prefab;
+	public int offset;
+	public int frequency;
+	public float magnitude;
+	public int Number_of_Cubes;
+	public bool Animation;
+	public bool In_Sync;
+	public float Vortex;
+	
+	private GameObject[] cubes;
+	private float timer;
+	private int count = 0;
+	private bool flag_sync = false;
+	private bool flag_animation = false;
+	
+    void Start()
+    {
+		cubes = new GameObject[Number_of_Cubes];
+
+		for (int i = 0; i < Number_of_Cubes; i++)
+		{
+			Vector3 offset3 = new Vector3(i * offset, 0, 0);
+			cubes[i] = Instantiate(Cube_prefab, transform.position + offset3, Quaternion.identity);
+		}
+			
+    }
+	
+
+    void Update()
+    {
+		if (Animation && In_Sync)
+		{
+			flag_animation = false;
+			if (!flag_sync)
+			{
+				Reset_Cubes();
+				timer = 0;
+				flag_sync = true;
+			}
+
+			timer += Time.deltaTime * 2;
+			for (int i = 0; i < Number_of_Cubes; i++)
+			{
+				cubes[i].transform.position += Vector3.up * Mathf.Sin(timer + Mathf.PI/2) * Time.deltaTime * magnitude;
+			}
+		}
+		else if (Animation)
+		{
+			flag_sync = false;
+
+			if (!flag_animation)
+			{
+				Reset_Cubes();
+				timer = 0;
+				flag_animation = true;
+			}
+			timer += Time.deltaTime * frequency;
+
+			if(((int)timer % 2 == 0) && (int)timer > 0 && count < Number_of_Cubes)
+			{
+				timer = 0;
+				cubes[count].GetComponent<SinCube>().animate = true;
+				cubes[count].GetComponent<SinCube>().vortex = (count + 1) * Vortex/10;
+				cubes[count].GetComponent<SinCube>().magnitude = magnitude;
+				count++;
+			}
+
+		}
+		else
+		{
+			flag_sync = false;
+			Reset_Cubes();
+			timer = 0;
+		}
+        
+    }
+
+	private void Reset_Cubes()
+	{
+		for (int i = 0; i < Number_of_Cubes; i++)
+		{
+			Vector3 offset3 = new Vector3(i * offset, 0, 0);
+			cubes[i].transform.position = transform.position + offset3;
+			cubes[i].GetComponent<SinCube>().animate = false;
+			count = 0;
+		}
+	}
+}
